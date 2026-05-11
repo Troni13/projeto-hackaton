415dc92b93b155ba2b194371716348c14df83489
Gabriel Troni <gabriel.troni13@gmail.com>
2026-05-11 13:03:57 -0300

    teste 2

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return "Banco MySQL conectado com sucesso! Teste"
+    return "Banco MySQL conectado com sucesso! Teste1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
